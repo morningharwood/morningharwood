4e21a9f530a2175f3e3b4b7a2f4a736a6a2c5985
Matthew Harwood <matthhar12@gmail.com>
2018-06-05 19:44:37 -0700

    first commit

---
+++ apps/morningharwood/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NxModule } from '@nrwl/nx';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserTransferStateModule,
+    RouterModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    NxModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ apps/morningharwood/src/app/app.server.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+const routes: Routes = [ { path: 'app-shell-path', component: AppShellComponent }];
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+    RouterModule.forRoot(routes),
+    ServerTransferStateModule,
+    ModuleMapLoaderModule
+  ],
+  bootstrap: [AppComponent],
+  declarations: [AppShellComponent],
+})
+export class AppServerModule {}
