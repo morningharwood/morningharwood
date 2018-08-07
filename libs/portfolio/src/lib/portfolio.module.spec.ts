173e5253283b37f38335287a267071a7da690ca7
Matthew Harwood <matthhar12@gmail.com>
2018-08-06 21:55:32 -0700

    Init of porfolio ui

---
+++ libs/portfolio/src/lib/portfolio.module.spec.ts
@@ -0,0 +1,14 @@
+import { async, TestBed } from '@angular/core/testing';
+import { PortfolioModule } from './portfolio.module';
+
+describe('PortfolioModule', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [PortfolioModule]
+    }).compileComponents();
+  }));
+
+  it('should create', () => {
+    expect(PortfolioModule).toBeDefined();
+  });
+});
